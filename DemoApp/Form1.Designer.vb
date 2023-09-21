2c6e1c995001159cc3cd1caa246f66dc13df4856
Afton Lawver <lawverap25@gmail.com>
2023-09-20 21:46:36 -0700

    Created my first form with button working with variables.

---
+++ DemoApp/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnVariables = New Button()
+        SuspendLayout()
+        ' 
+        ' btnVariables
+        ' 
+        btnVariables.BackColor = SystemColors.ActiveCaption
+        btnVariables.Location = New Point(333, 121)
+        btnVariables.Name = "btnVariables"
+        btnVariables.Size = New Size(167, 53)
+        btnVariables.TabIndex = 0
+        btnVariables.Text = "Variables Demonstration"
+        btnVariables.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnVariables)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnVariables As Button
+End Class
---
+++ DemoApp/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnVariables_Click(sender As Object, e As EventArgs) Handles btnVariables.Click
+
+        Dim stFirstName As String
+        Dim stLastName As String
+
+        stFirstName = "Afton"
+        stLastName = "Lawver"
+
+        MessageBox.Show("Hello " & stFirstName & " " & stLastName)
+
+    End Sub
+End Class
